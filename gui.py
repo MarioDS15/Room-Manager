ff3f69fedde2cd547537486601d8c5d1c20fe07d
MarioDS15 <hmzainal@gmail.com>
2023-12-28 17:49:37 +0300

    first update

--- gui.py
+++ gui.py
@@ -6,14 +6,67 @@ class Application:
         self.root = root
         self.root.title("Room Entry Logger")
 
-        # Create and pack labels and entry fields
+        # Create the data display first
+ 
 
-        self.log_button = tk.Button(self.root, text="Log Entry", command=self.log)
-        self.log_button.pack()
+        # Create a frame for the log entry widgets on the right
+        self.entry_frame = tk.Frame(self.root)
+        self.entry_frame.pack(side=tk.LEFT, fill=tk.BOTH)
 
-        self.data_display = tk.Text(self.root, height=10, width=40)
-        self.data_display.pack()
+        self.label = tk.Label(self.entry_frame, text="Enter Name:")
+        self.label.pack()
 
+        # Create the name entry inside the frame
+        self.name_entry = tk.Entry(self.entry_frame)
+        self.name_entry.pack()
+
+        self.idLabel = tk.Label(self.entry_frame, text="Enter ID number:")
+        self.idLabel.pack()
+
+        # Create the ID entry inside the frame
+        self.id_entry = tk.Entry(self.entry_frame)
+        self.id_entry.pack(padx=10)
+
+
+        self.items_frame = tk.Frame(self.entry_frame)
+        self.items_frame.pack(fill=tk.X, padx=10, pady=10)  # Adjust padding as needed
+
+        
+        self.itemLabel = tk.Label(self.items_frame, text="Items being checked out:")
+        self.itemLabel.pack(padx=3, pady=5)
+
+        self.keyboard = tk.BooleanVar()
+        self.keyboardWidget = tk.Checkbutton(self.items_frame, text="Keyboard", variable=self.keyboard)
+        self.keyboardWidget.pack(anchor='w')
+
+        self.mouse = tk.BooleanVar()
+        self.mouseWidget = tk.Checkbutton(self.items_frame, text="Mouse", variable=self.mouse)
+        self.mouseWidget.pack(anchor='w')
+
+        self.headset = tk.BooleanVar()
+        self.headsetWidget = tk.Checkbutton(self.items_frame, text="Headset", variable=self.headset)
+        self.headsetWidget.pack(anchor='w')
+
+        self.controller = tk.BooleanVar()
+        self.controllerWidget = tk.Checkbutton(self.items_frame, text="Controller", variable=self.controller)
+        self.controllerWidget.pack(anchor='w')
+
+
+        self.checkedframe = tk.Frame(self.root)
+        self.checkedframe.pack(fill=tk.X)  # Adjust padding as needed
+        self.checkedInLabel = tk.Label(self.checkedframe, text="Items being checked out:")
+        self.checkedInLabel.pack(padx=3, pady=5)
+        self.data_display = tk.Text(self.checkedframe, height=10, width=40)
+        self.data_display.pack(side=tk.RIGHT, padx=10)
+
+        
+
+
+        # Create and pack the log button inside the frame
+        self.log_button = tk.Button(self.entry_frame, text="Log student", command=self.log)
+        self.log_button.pack(padx=10, pady=10)
+
+        # Update the display with current data
         self.update_display()
 
     def log(self):
@@ -25,7 +78,3 @@ class Application:
         self.data_display.delete(1.0, tk.END)
         display_text = update_display()
         self.data_display.insert(tk.END, display_text)
-
-    def remove(self, index):
-        remove_entry(index)
-        self.update_display()
